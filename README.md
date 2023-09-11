c3ddb19666e98cbf877942802354c079ddabeddb
arcones <marta.arcones@alumnos.upm.es>
2023-09-11 20:52:31 +0200

    Feature/mvp (#1)
    Add API connection & CI script

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # kilombo
+
+[![Build and test](https://github.com/arcones/kilombo/actions/workflows/build-and-test.yml/badge.svg)](https://github.com/arcones/kilombo/actions/workflows/build-and-test.yml)
+
 Automate NIH search &amp; aggregation
--- kilombo/core.py
+++ kilombo/core.py
@@ -1,7 +1,20 @@
-import requests, untangle
+import requests
+import xmltodict
 
-response = requests.get("https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=gds&term=multiple%20sclerosis%20AND%20rna%20seq")
+NCBI_E_UTILITIES_GDS_BASE_URL = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=gds"  # TODO ask claudivó if gds will be the db forever (there are more!)
 
-obj = untangle.parse(response.content)
+EXAMPLE_QUERY = "multiple sclerosis AND rna seq"
 
-print(obj)
+main_query_response = requests.get(f"{NCBI_E_UTILITIES_GDS_BASE_URL}&term={EXAMPLE_QUERY}")
+
+main_query_response_parsed = xmltodict.parse(main_query_response.text)
+
+paper_ids = main_query_response_parsed["eSearchResult"]["IdList"]["Id"]
+
+paper_response = requests.get(
+    f"https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=gds&id=200239875&api_key=ed06bd0f3c27a605d87e51e94eecab115908"
+)
+
+paper_response_parsed = xmltodict.parse(paper_response.text)
+
+print(paper_response_parsed)
