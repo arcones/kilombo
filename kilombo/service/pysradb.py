f1b8329d4d97dc8cf9d6c126b831b30e5960daf2
Marta Arcones Rodríguez <marta.arconesrodriguez@zooplus.com>
2023-10-19 17:39:03 +0200

    WIP

--- kilombo/service/pysradb.py
+++ kilombo/service/pysradb.py
@@ -5,32 +5,31 @@ from pysradb import SRAweb
 
 
 async def add_sra_study_accessions(id_to_gse_dict: dict):
+    tasks = {}
     responses = {}
 
     for study_id, geo_accession_id in id_to_gse_dict.items():
+        logging.info(f"Extracting from geo accession id {geo_accession_id} for study {study_id} the SRP info...")
+        srp_info = asyncio.create_task(_pysradb_gse_to_srp(geo_accession_id))
+        tasks[study_id] = srp_info, geo_accession_id
+        logging.info(f"Done extracting from geo accession id {geo_accession_id} for study {study_id} the SRP info")
+
+    all_tasks = asyncio.all_tasks()
+    all_tasks_filtered = list(
+        filter(lambda task: "_pysradb_gse_to_srp" in f"${task.get_coro().cr_code}", all_tasks)
+    )  ## Las tengo en un array asiq quiza puedo esperar por ellas sin filtros
+    await asyncio.wait(all_tasks_filtered)
+
+    for task in tasks:
         try:
-            logging.info(f"Extracting from geo accession id {geo_accession_id} for study {study_id} the SRP info...")
-            srp_info = asyncio.create_task(_pysradb_gse_to_srp(geo_accession_id))
-            responses[study_id] = srp_info, geo_accession_id
-            logging.info(f"Done extracting from geo accession id {geo_accession_id} for study {study_id} the SRP info")
+            result = tasks[task][0].result()["study_accession"][0]
+            responses[task] = {"geo_accession_id": tasks[task][1], "sra_study_accession": result}
         except Exception as exception:
             logging.error(f"PYSRADB response not expected for study id {study_id} and geo accession {geo_accession_id}")
             logging.error(f"Exception {exception.__class__.__name__} with {exception.args}")
-
-    all_tasks = asyncio.all_tasks()
-    all_tasks_filtered = list(filter(lambda task: "_pysradb_gse_to_srp" in f"${task.get_coro().cr_code}", all_tasks))
-    await asyncio.wait(all_tasks_filtered)
-
-    for response in responses:
-        try:
-            result = responses[response][0].result()["study_accession"][0]
-            responses[response] = {"geo_accession_id": responses[response][1], "sra_study_accession": result}
-        except AttributeError:
-            logging.error(f"Problemas con el {responses[response][1]} de donde no se puede sacar el SRP")
-            responses[response] = {"geo_accession_id": responses[response][1], "sra_study_accession": "Unknown"}
 
     return responses
 
 
 async def _pysradb_gse_to_srp(geo_accession_id: str):
-    return SRAweb().gse_to_srp(geo_accession_id)
+    return SRAweb().gse_to_srp(geo_accession_id)  # https://pythonexamples.org/pandas-check-if-dataframe-is-empty/
